2865bf19e567b70beb34c0c5c6127bad19a42089
Leonardo-Moreno <leonardo.mlmendes@gmail.com>
2021-11-16 20:05:31 -0300

    Adicionar arquivos de projeto.

---
+++ Testebot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Testebot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path1 = (@"C:\Users\Downloads");
+            string path2 = (@"C:\Users\\OneDrive\Imagens\BotFolder\");
+
+            string[] typeFile = new string[3]{ "*.jpg", "*.png", "*.jpeg" };
+
+            for (int i = 0; i < typeFile.Length; i++)
+            {
+                foreach (string file in Directory.EnumerateFiles(path1, typeFile[i]))
+                {
+                    string contents = File.ReadAllText(file);
+                    File.Copy(file, path2 + Path.GetFileName(file));
+                    File.Delete(file);
+
+                }
+            }
+
+        }
+    }
+}
